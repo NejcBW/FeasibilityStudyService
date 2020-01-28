713e01b7d93b37fb97c9149965187f8b49d64491
Nejc Brelih-Wasowski <nejcbw@protonmail.com>
2020-01-28 22:10:05 +0100

    Chart data loadind fixed

--- Server/wwwroot/scripts/index.js
+++ Server/wwwroot/scripts/index.js
@@ -1,18 +1,34 @@
 var epwState;
 var dsx;
+var ctx = document.getElementById('myChart1').getContext('2d');
+var myChart;
 
 function foo() {
-    var t0 = performance.now();
     fetch("https://localhost:5001/EPW")
         .then(response => response.json())
         .then(data => {
             epwState = data;
             dsx = data.data.map(p => ({ x: p.id, y: p.dryBulb }));
-            var t1 = performance.now();
-            var dt = t1 - t0;
-            console.log("EPW fetching and loading time:" + dt)
+            myChart = chartLoad();
         })
         .catch(err => console.log("Something went wrong"));
+}
+
+function chartLoad() {
+
+    var chart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: 'dataset 1',
+                data: dsx
+            }]
+        },
+        options: {}
+    });
+
+    return chart;
+
 }
 
 foo();
